68e878022a9901eacc7fd94fbe0a31641e220803
nymwa <nymwa0@gmail.com>
2020-04-13 22:19:45 +0900

    ch08

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,6 @@
 wget https://nlp100.github.io/data/popular-names.txt
 wget https://nlp100.github.io/data/jawiki-country.json.gz
 wget https://nlp100.github.io/data/neko.txt
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00359/NewsAggregatorDataset.zip
+wget http://download.tensorflow.org/data/questions-words.txt
+wget http://www.gabrilovich.com/resources/data/wordsim353/wordsim353.zip
